56bfe5750dc277b2aff4bc95780930f14a16575b
IgorFilin <filinigor@yandex.ru>
2023-02-14 17:45:58 +0300

    Commit 1

--- src/otherTask/trainig.js
+++ src/otherTask/trainig.js
@@ -1,2 +1,44 @@
+const data = [
+    {
+        name: 'art',
+        type: '1',
+    },
+    {
+        name: 'art2',
+        type: '2',
+    },
+    {
+        name: 'art2',
+        type: '3',
+    },
+    {
+        name: 'art3',
+        type: '1',
+    },
+    {
+        name: 'art4',
+        type: '2',
+    },
+    {
+        name: 'art5',
+        type: '3',
+    },
+];
+
+const newObj = data.reduce((acc,curr,i)=> {
+      acc[curr.type].push(curr)
+}, {
+    '1': [],
+    '2': [],
+    '3': []
+})
 
 
+
+
+
+const res = {
+    "1": [{}]
+     "2":
+     "3":
+}
