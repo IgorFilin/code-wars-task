060440ec6bb425f74bc0eb2dd84540b821966983
IgorFilin <filinigor@yandex.ru>
2023-05-24 16:17:11 +0300

    feat

--- src/DoneTasks/78)6kyu.js
+++ src/DoneTasks/78)6kyu.js
@@ -1,1 +1,13 @@
-https://www.codewars.com/kata/5842df8ccbd22792a4000245/train/javascript
+// Напишите число в развернутой форме
+// Вам будет дано число, и вам нужно будет вернуть его в виде строки в расширенной форме . Например:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// ПРИМЕЧАНИЕ. Все числа будут целыми числами больше 0.
+
+function expandedForm(num) {
+  // Your code here
+}
+
+console.log(expandedForm(70304));
