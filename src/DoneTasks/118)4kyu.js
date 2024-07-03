e3b247c89dde0294cdd40f92fd6ad8611eaf645f
IgorFilin <filinigor@yandex.ru>
2024-07-03 23:01:31 +0300

    Попытка решения задачи на подбор пароля

--- src/DoneTasks/118)4kyu.js
+++ src/DoneTasks/118)4kyu.js
@@ -1,80 +1,67 @@
-// Хорошо, детектив, один из наших коллег успешно заметил нашу жертву, грабителя Робби. 
+// Хорошо, детектив, один из наших коллег успешно заметил нашу жертву, грабителя Робби.
 // Мы последовали за ним на секретный склад, где и предполагаем найти все украденные вещи.
-//  Дверь этого склада защищена электронным кодовым замком. К сожалению, наш шпион не уверен в ПИН-коде, который он видел, 
-//  когда Робби ввел его. Он заметил PIN-код 1357, но также сказал, что это возможно, 
+//  Дверь этого склада защищена электронным кодовым замком. К сожалению, наш шпион не уверен в ПИН-коде, который он видел,
+//  когда Робби ввел его. Он заметил PIN-код 1357, но также сказал, что это возможно,
 //  что каждая из увиденных им цифр на самом деле могла быть другой соседней цифрой (по горизонтали или вертикали, но не по диагонали).
 //   Например. вместо 1 это может быть 2 или 4. А вместо 5 это может быть 2, 4, 6 или 8. Он также упомянул, что знает такие замки.
-//    Вы можете ввести неограниченное количество неправильных PIN-кодов, 
-//    они никогда окончательно не заблокируют систему и не подадут сигнал тревоги. 
-//    Вот почему мы можем опробовать все возможные (*) варианты. * возможно в смысле: 
+//    Вы можете ввести неограниченное количество неправильных PIN-кодов,
+//    они никогда окончательно не заблокируют систему и не подадут сигнал тревоги.
+//    Вот почему мы можем опробовать все возможные (*) варианты. * возможно в смысле:
 //    самого наблюдаемого PIN-кода и всех его вариантов с учетом соседних цифр. Можете ли вы помочь нам найти все эти варианты?
 //  Было бы неплохо иметь функцию, возвращающую массив (или список в Java).
 
 function getPINs(observed) {
   // TODO: This is your job, detective!
-  const resultVariants = []
+  const resultVariants = [];
+
   const variantNumbers = [
-    ['1','2','3'],
-    ['4','5','6'],
-    ['7','8','9'],
-    ['0'],
-  ]
-  const arr = observed.split(' ')
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+    ["", "0", ""],
+  ];
+  const arr = observed.split(" ");
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < variantNumbers.length; j++) {
-        if(variantNumbers[j].includes(arr[i])) {
+  function findNumbers(matrix, startX, startY) {
+    const queue = [[startX, startY]];
+    const numbersFound = [];
 
+    queue.push([startX - 1, startY]);
+    queue.push([startX + 1, startY]);
+    queue.push([startX, startY - 1]);
+    queue.push([startX, startY + 1]);
+
+    while (queue.length > 0) {
+      const [x, y] = queue.shift();
+      if (matrix[x] && matrix[x][y]) {
+        if (matrix[x][y] !== "") {
+          numbersFound.push(matrix[x][y]);
         }
+      }
     }
+
+    return numbersFound;
   }
 
+  function searchCoords(number, matrix) {
+    const coords = {
+      x: null,
+      y: null,
+    };
 
+    matrix.forEach((arr, index) => {
+      if (arr.includes(String(number))) {
+        coords.x = index;
+        coords.y = arr.findIndex((el) => el === String(number));
+      }
+    });
 
+    return coords;
+  }
+
+  function searchAllCombination() {}
+
+  const coords = searchCoords();
 }
 
-console.log(getPINs('8'));
-// изучить пример
-// function findNumbers(matrix, startX, startY) {
-//   const rows = matrix.length;
-//   if (rows === 0) return []; // Пустая матрица
-
-//   const cols = matrix[0].length;
-//   if (cols === 0) return []; // Матрица с пустыми строками
-
-//   const visited = new Array(rows).fill(false).map(() => new Array(cols).fill(false));
-//   const queue = [[startX, startY]];
-//   const numbersFound = [];
-
-//   while (queue.length > 0) {
-//       const [x, y] = queue.shift();
-
-//       // Проверяем, что текущая ячейка не посещалась и что координаты в пределах матрицы
-//       if (!visited[x][y]) {
-//           visited[x][y] = true;
-//           numbersFound.push(matrix[x][y]);
-
-//           // Проверяем соседние ячейки (вверх, вниз, влево, вправо)
-//           if (x > 0) queue.push([x - 1, y]); // вверх
-//           if (x < rows - 1) queue.push([x + 1, y]); // вниз
-//           if (y > 0) queue.push([x, y - 1]); // влево
-//           if (y < cols - 1) queue.push([x, y + 1]); // вправо
-//       }
-//   }
-
-//   return numbersFound;
-// }
-
-// // Пример использования
-// const matrix = [
-//   ['1','2','3'],
-//   ['4','5','6'],
-//   ['7','8','9'],
-//   ['0']
-// ];
-
-// const startX = 1;
-// const startY = 1;
-
-// const result = findNumbers(matrix, startX, startY);
-// console.log('Найденные числа:', result); 
+console.log(getPINs("369"));
